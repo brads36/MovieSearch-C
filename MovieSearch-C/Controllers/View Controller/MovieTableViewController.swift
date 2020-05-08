65f02acd732d23df522450f2aa573e17c2f5a771
Bryce Bradshaw <brycewbradshaw@icloud.com>
2020-05-08 11:38:57 -0600

    Completed, need to fix a few Storyboard issues

--- MovieSearch-C/Controllers/View Controller/MovieTableViewController.swift
+++ MovieSearch-C/Controllers/View Controller/MovieTableViewController.swift
@@ -10,76 +10,51 @@ import UIKit
 
 class MovieTableViewController: UITableViewController {
 
+    // MARK: - Outlets
+    @IBOutlet weak var movieSearchBar: UISearchBar!
+    
+    // MARK: - Properties
+    var movies: [WBBMovie] = []
+    
+    // MARK: - LifeCycles
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        movieSearchBar.delegate = self
     }
 
     // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        
+        return self.movies.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "movieCell", for: indexPath) as? MovieTableViewCell else { return UITableViewCell()}
 
-        // Configure the cell...
+        let movie = self.movies[indexPath.row]
+        cell.movie = movie
 
         return cell
     }
-    */
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+} // End of class
+
+extension MovieTableViewController: UISearchBarDelegate {
+     
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let searchTerm = searchBar.text, !searchTerm.isEmpty else { return }
+        
+        WBBMovieController.fetchMovies(forSearchTerm: searchTerm) { (result) in
+            DispatchQueue.main.async {
+                switch result {
+                    
+                case .none:
+                    print("There was an error when searching for movies.")
+                case .some(let movies):
+                    self.movies = movies
+                    self.tableView.reloadData()
+                }
+            }
+        }
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
